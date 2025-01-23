4c64cc378508e332fc77a906e40a5fcbc6837a2d
YeoniSon <sjy8211@gmail.com>
2025-01-23 22:04:57 +0900

    problem1000 문제,풀이 2가지 입출력 방식 활용

---
+++ BackJoon/problem1000/Main.java
@@ -0,0 +1,22 @@
+package BackJoon.problem1000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        Scanner sc = new Scanner(System.in);      //Scanner 방식
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A+B);
+    }
+}
