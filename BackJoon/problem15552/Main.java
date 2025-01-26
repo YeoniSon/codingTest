25df81d77b0652b9f7fbc07490d1e0f6f2b1f1b1
YeoniSon <sjy8211@gmail.com>
2025-01-26 20:09:19 +0900

    problem15552 문제,풀이

---
+++ BackJoon/problem15552/Main.java
@@ -0,0 +1,24 @@
+package BackJoon.problem15552;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a, b;
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            bw.write(a + b + "\n");
+        }
+        bw.close();
+    }
+}
