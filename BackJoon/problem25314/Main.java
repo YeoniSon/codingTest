4c46db9446a687f4ece79135e44f098f0593bd0c
YeoniSon <sjy8211@gmail.com>
2025-01-26 20:00:21 +0900

    problem25314 문제,풀이

---
+++ BackJoon/problem25314/Main.java
@@ -0,0 +1,20 @@
+package BackJoon.problem25314;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int x = n / 4;
+
+        for (int i = 0; i < x; i++) {
+            System.out.print("long ");
+        }
+        System.out.print("int");
+    }
+}
