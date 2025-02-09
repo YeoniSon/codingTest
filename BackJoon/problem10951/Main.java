f950ea28f7b42bed00b02dae1f1440f00e9f0818
YeoniSon <sjy8211@gmail.com>
2025-02-10 01:07:01 +0900

    problem10951 문제,풀이

---
+++ BackJoon/problem10951/Main.java
@@ -0,0 +1,18 @@
+package BackJoon.problem10951;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        while (sc.hasNextInt()){
+            int a, b;
+            a = sc.nextInt();
+            b = sc.nextInt();
+
+            System.out.println(a + b);
+        }
+        sc.close();
+    }
+}
