a0ce55857739be004d080ad263707220ef5ad94a
YeoniSon <sjy8211@gmail.com>
2025-02-10 01:34:43 +0900

    problem10871 문제,풀이

---
+++ BackJoon/problem10871/Main.java
@@ -0,0 +1,29 @@
+package BackJoon.problem10871;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n, x;
+        n = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int value = Integer.parseInt(st.nextToken());
+
+            if (value < x) {
+                sb.append(value).append(' ');
+            }
+        }
+        System.out.println(sb);
+    }
+}
