dd29267ac6ba4329204a9e7d6fdda89c502e8d34
YeoniSon <sjy8211@gmail.com>
2025-02-10 23:45:24 +0900

    problem5597 문제,풀이

--- BackJoon/problem5597/Main.java
+++ BackJoon/problem5597/Main.java
@@ -1,21 +1,39 @@
 package BackJoon.problem5597;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+//        Scanner scan = new Scanner(System.in);
+//
+//        int[] student = new int[30];
+//
+//        for (int i = 0; i < 28; i++) {
+//            int n = scan.nextInt();
+//            student[n-1] = 1;
+//        }
+//
+//        for (int i = 0; i < student.length; i++) {
+//            if(student[i] != 1){
+//                System.out.println(i+1);
+//            }
+//        }
 
-        int[] student = new int[30];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean[] student = new boolean[30];
 
         for (int i = 0; i < 28; i++) {
-            int n = scan.nextInt();
-            student[n-1] = 1;
+            int n = Integer.parseInt(br.readLine());
+            student[n-1] = true;
         }
 
         for (int i = 0; i < student.length; i++) {
-            if(student[i] != 1){
-                System.out.println(i+1);
+            if (!student[i]) {
+                System.out.println(i + 1);
             }
         }
     }
