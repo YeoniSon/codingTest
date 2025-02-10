838eda25235aff5ca38bd75f2cecffd4f3c9a918
YeoniSon <sjy8211@gmail.com>
2025-02-10 17:04:37 +0900

    problem10813 문제,풀이

---
+++ BackJoon/problem10813/Main.java
@@ -0,0 +1,47 @@
+package BackJoon.problem10813;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static int[] buckets;
+    static StringBuilder sb = new StringBuilder();
+
+    static void swapBall(int start, int end) {
+        int tmp = buckets[start];
+        buckets[start] = buckets[end];
+        buckets[end] = tmp;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        buckets = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            buckets[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            swapBall(start, end);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(buckets[i]).append(" ");
+        }
+        System.out.println(sb);
+
+    }
+}
