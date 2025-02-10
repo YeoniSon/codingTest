f950f511a3dace6b9df99c2233a75057324a6a91
YeoniSon <sjy8211@gmail.com>
2025-02-10 23:58:29 +0900

    problem3052 문제,풀이

--- BackJoon/problem3052/Main.java
+++ BackJoon/problem3052/Main.java
@@ -1,17 +1,30 @@
 package BackJoon.problem3052;
 
+//import java.util.HashSet;
+//import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+//        Scanner sc = new Scanner(System.in);
+//        HashSet<Integer> hs = new HashSet<Integer>();
+//
+//        for (int i = 0; i < 10; i++) {
+//            hs.add(sc.nextInt()%42);
+//        }
+//
+//        System.out.println(hs.size());
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         HashSet<Integer> hs = new HashSet<Integer>();
 
         for (int i = 0; i < 10; i++) {
-            hs.add(sc.nextInt()%42);
+            hs.add(Integer.parseInt(br.readLine())%42);
         }
-
         System.out.println(hs.size());
     }
 }
