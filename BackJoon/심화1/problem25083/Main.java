68f59af4f663d69ab257a8991c8207eb389377d8
YeoniSon <sjy8211@gmail.com>
2025-03-19 17:04:39 +0900

    problem25083 문제, 풀이

---
+++ BackJoon/심화1/problem25083/Main.java
@@ -0,0 +1,12 @@
+package BackJoon.심화1.problem25083;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7 ");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
--- BackJoon/심화1/problem25083/problem25083.java
+++
@@ -1,4 +0,0 @@
-package BackJoon.심화1.problem25083;
-
-public class problem25083 {
-}
