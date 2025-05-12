49b2dc4a895c63b0f2f00f79bd70185faa72f796
YeoniSon <sjy8211@gmail.com>
2025-05-12 14:57:25 +0900

    SWEA problem2047 문제, 풀이

---
+++ SWEA/D1/problem2047/Solution.java
@@ -0,0 +1,15 @@
+package SWEA.D1.problem2047;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        System.out.println(s.toUpperCase());
+    }
+}
