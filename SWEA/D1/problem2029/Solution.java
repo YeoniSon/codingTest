8f2d0096525859c7aa3ab54b36b8c1572bfdf675
YeoniSon <sjy8211@gmail.com>
2025-05-12 15:39:22 +0900

    SWEA problem2029 문제, 풀이

---
+++ SWEA/D1/problem2029/Solution.java
@@ -0,0 +1,23 @@
+package SWEA.D1.problem2029;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.printf("#%d %d %d\n", i, a/b, a%b);
+        }
+    }
+}
