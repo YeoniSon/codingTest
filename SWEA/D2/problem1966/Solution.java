0ef6aacee0eeaf2a9631c6bcccfc4474ef5201a6
YeoniSon <sjy8211@gmail.com>
2025-05-15 18:42:22 +0900

    SWEA problem1966 문제, 풀이

---
+++ SWEA/D2/problem1966/Solution.java
@@ -0,0 +1,33 @@
+package SWEA.D2.problem1966;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            int n = Integer.parseInt(br.readLine());
+
+            int[] arr = new int[n];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arr);
+
+            System.out.print("#" + tc + " ");
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
