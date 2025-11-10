d558f4348163b8c2e343dd98ccbd79890d456f9c
YeoniSon <sjy8211@gmail.com>
2025-11-10 21:36:08 +0900

    problem15964 문제 풀이

--- BackJoon/새싹/problem15964/Main.java
+++ BackJoon/새싹/problem15964/Main.java
@@ -1,4 +1,18 @@
 package BackJoon.새싹.problem15964;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Long A = Long.parseLong(st.nextToken());
+        Long B = Long.parseLong(st.nextToken());
+
+        System.out.println((A+B) * (A-B));
+    }
 }
