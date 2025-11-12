8354c4ac0eac338af9d6599a0912c5b8c3e03e88
YeoniSon <sjy8211@gmail.com>
2025-11-12 22:47:15 +0900

    problem11651 문제 풀이

---
+++ BackJoon/class2/problem11651/Main.java
@@ -0,0 +1,39 @@
+package BackJoon.class2.problem11651;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] s1, int[] s2) {
+                if (s1[1] == s2[1]) {
+                    return s1[0] - s2[0];
+                }else {
+                    return s1[1] - s2[1];
+                }
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
