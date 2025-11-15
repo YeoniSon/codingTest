c7ada22f87e734b73c097b26d662da0c9879d773
YeoniSon <sjy8211@gmail.com>
2025-11-15 17:59:14 +0900

    problem11047 문제 풀이

---
+++ BackJoon/class3/problem11047/Main.java
@@ -0,0 +1,29 @@
+package BackJoon.class3.problem11047;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int cnt = 0;
+        while (K != 0) {
+            if (K / arr[N-1] > 0) {
+                cnt += K / arr[N-1];
+                K = K % arr[N-1];
+            }
+            N--;
+        }
+
+        System.out.println(cnt);
+    }
+}
