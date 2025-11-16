fb9c131d8348f0a14c74bd87e83a5056f191dc0f
YeoniSon <sjy8211@gmail.com>
2025-11-17 00:11:40 +0900

    problem17219 문제 풀이

---
+++ BackJoon/class3/problem17219/Main.java
@@ -0,0 +1,29 @@
+package BackJoon.class3.problem17219;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, String> pw = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            pw.put(st.nextToken(), st.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+            bw.write(pw.get(br.readLine()));
+            bw.write("\n");
+        }
+
+        bw.flush();
+    }
+}
